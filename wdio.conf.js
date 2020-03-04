edf556c1cca75d76c0ad19563e0903fd105a5248
Decio Ferreira <decio.ferreira@decioferreira.com>
2020-03-04 23:17:21 +0000

    goog:chromeOptions

--- wdio.conf.js
+++ wdio.conf.js
@@ -59,8 +59,15 @@ exports.config = {
     {
       maxInstances: 5,
       browserName: "chrome",
-      chromeOptions: {
-        args: ["headless", "disable-gpu", "no-sandbox"]
+      "goog:chromeOptions": {
+        // to run chrome headless the following flags are required
+        // (see https://developers.google.com/web/updates/2017/04/headless-chrome)
+        args: [
+          "--headless",
+          "--disable-gpu",
+          // Running as root without --no-sandbox is not supported. See https://crbug.com/638180.
+          "--no-sandbox"
+        ]
       }
     }
   ],
